74bdf725b79ce1369a8c92fa01bfc65901536f29
Sergey Mayorov <majoroff.cc@gmail.com>
2016-03-01 19:25:59 +0300

    initial

---
+++ blog/models.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+
+import uuid
+from py2neo import Graph, Node, Relationship
+from passlib.hash import bcrypt
+from datetime import datetime
+
+
+graph = Graph()
+
+class User:
+
+    def __init__(self, username):
+        self.username = username
+
+    def find(self):
+        user = graph.find_one("User", "username", self.username)
+        return user
+
+    def register(self, password):
+        if not self.find():
+            user = Node("User", username=self.username, password = bcrypt.encrypt(password))
+            graph.create(user)
+            return True
+        
+        return False
+
+    def verify_password(self, password):
+        user = self.find()
+        if not user:
+            return False
+
+        return bcrypt.verify(password, user['password'])
+
+    def add_post(self, title, tags, text):
+        user = self.find()
+        post = Node(
+            "Post", 
+            id=str(uuid.uuid4()),
+            title=title,
+            text=text,
+            timestamp=int(datetime.now().strftime("%s")),
+            date=datetime.now().strftime("%F")
+        )
+        rel = Relationship(user, "PUBLISHED", post)
+        graph.create(rel)
+
+        tags = [x.strip() for x in tags.lower().split(",")]
+        tags = set(tags)
+
+        for tag in tags:
+            t = graph.merge_one("Tag", "name", tag)
+            rel = Relationship(t, "TAGGED", post)
+            graph.create(rel)
+
+
+    def like_post(self, post_id):
+        user = self.find()
+        post = graph.find_one("Post", "id", post_id)
+        graph.create_unique(Relationship(user, "LIKES", post))
+
+
+def todays_recent_posts(n):
+    query = """
+        MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+        WHERE post.date = {today}
+        RETURN user.username AS username, post, COLLECT(tag.name) AS tags
+        ORDER BY post.timestamp DESC LIMIT {n}
+    """
+    today = datetime.now().strftime("%F")
+    return graph.cypher.execute(query, today=today, n=n)
