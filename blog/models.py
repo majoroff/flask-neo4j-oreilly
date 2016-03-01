e7739b483060bc0c5ea02e8ccae3dc4b12c9652a
Sergey Mayorov <majoroff.cc@gmail.com>
2016-03-02 00:22:19 +0300

    finished

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,14 @@
 
 import uuid
 from py2neo import Graph, Node, Relationship
+from py2neo.ext.calendar import GregorianCalendar
 from passlib.hash import bcrypt
 from datetime import datetime
 
 
 graph = Graph()
+calendar = GregorianCalendar(graph)
+
 
 class User:
 
@@ -34,17 +37,20 @@ class User:
 
     def add_post(self, title, tags, text):
         user = self.find()
+        today = datetime.now()
         post = Node(
             "Post", 
             id=str(uuid.uuid4()),
             title=title,
             text=text,
-            timestamp=int(datetime.now().strftime("%s")),
-            date=datetime.now().strftime("%F")
+            timestamp=int(today.strftime("%s")),
+            date=today.strftime("%F")
         )
         rel = Relationship(user, "PUBLISHED", post)
         graph.create(rel)
 
+        today_node = calendar.date(today.year, today.month, today.day).day
+        graph.create(Relationship(post, "ON", today_node))
         tags = [x.strip() for x in tags.lower().split(",")]
         tags = set(tags)
 
@@ -58,6 +64,47 @@ class User:
         user = self.find()
         post = graph.find_one("Post", "id", post_id)
         graph.create_unique(Relationship(user, "LIKES", post))
+
+    def recent_posts(self, n):
+        query = """
+            MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+            WHERE user.username = {username}
+            RETURN post, COLLECT(tag.name) AS tags
+            ORDER BY post.timestamp DESC LIMIT {n}
+
+        """
+        return graph.cypher.execute(query, username=self.username, n=n)
+
+    def similar_users(self, n):
+        query = """
+            MATCH (user1:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                  (user2:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+            WHERE user1.username = {username} AND user1 <> user2
+            WITH user2, COLLECT(DISTINCT tag.name) AS tags, 
+            COUNT(DISTINCT tag.name) AS tag_count
+            ORDER BY tag_count DESC LIMIT {n}
+            RETURN user2.username AS similar_user, tags
+        """
+        return graph.cypher.execute(query, username=self.username, n=n)
+
+    def commonality_of_user(self, user):
+        query1 =  """
+            MATCH (user1:User)-[:PUBLISHED]->(post:Post)<-[:LIKES]-(user2:User)
+            WHERE user1.username = {username1} AND user2.username = {username2}
+            RETURN COUNT(post) AS likes
+
+        """
+        likes = graph.cypher.execute_one(query1, username1 = self.username, username2 = user.username) or 0
+
+        query2 = """
+            MATCH (user1:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                  (user2:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+            WHERE user1.username = {username1} AND user2.username = {username2}
+            RETURN COLLECT(DISTINCT tag.name) AS tags  
+        """
+
+        tags = graph.cypher.execute(query2, username1 = self.username, username2 = user.username)[0]["tags"]
+        return {"likes":likes, "tags":tags}
 
 
 def todays_recent_posts(n):
